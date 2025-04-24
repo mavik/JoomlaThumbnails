e99e1ab65f536b758fcd1dfd72e8808ac6ceb82e
Vitalii Marenkov <admin@mavik.com.ua>
2025-04-24 21:44:53 +0200

    The first working versin

--- mavik-thumbnails/plugin/src/Extension/ImagesReplacer.php
+++ mavik-thumbnails/plugin/src/Extension/ImagesReplacer.php
@@ -5,11 +5,14 @@ namespace Mavik\Plugin\Content\Thumbnails\Extension;
 defined('_JEXEC') or die;
 
 use Mavik\Thumbnails\Thumbnails;
+use Mavik\Thumbnails\JsAndCss;
 use Mavik\Thumbnails\Configuration;
 use Mavik\Thumbnails\Configuration\Base as ConfBase;
 use Mavik\Thumbnails\Configuration\Server as ConfServer;
 use Joomla\CMS\Uri\Uri;
 use Joomla\Registry\Registry;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Application\CMSWebApplicationInterface;
 
 class ImagesReplacer
 {
@@ -35,9 +38,36 @@ class ImagesReplacer
 
     public function process(array $text)
     {
-        foreach($text as &$textItemm) {
-            $textItemm = ($this->thumbnails)($textItemm)->html;
+        $jsAndCss = new JsAndCss();
+        foreach($text as &$textItem) {
+            $result = ($this->thumbnails)($textItem);
+            $textItem = $result->html;
+            $jsAndCss->merge($result->jsAndCss);
         }
+        $this->registerJsAndCss($jsAndCss);
         return $text;
+    }
+
+    private function registerJsAndCss(JsAndCss $jsAndCss): void
+    {
+        $app = Factory::getApplication();
+        if (!$app instanceof CMSWebApplicationInterface) {
+            return;
+        }
+        $wa = $app->getDocument()->getWebAssetManager();
+        foreach ($jsAndCss->js() as $js) {
+            $jsName = "lib_mavik_thumbnails.popup";
+            $wa->registerAndUseScript($jsName, "media/lib_mavik_thumbnails/{$js}", ['version' => 'auto']);
+        }
+        $inlineJs = implode("\n", $jsAndCss->inlineJs());
+        $wa->addInlineScript("
+            document.addEventListener('DOMContentLoaded', function () {
+                {$inlineJs}
+            });
+        ");
+        foreach ($jsAndCss->css() as $css) {
+            $cssName = "lib_mavik_thumbnails.popup";
+            $wa->registerAndUseStyle($cssName, "media/lib_mavik_thumbnails/{$css}", ['version' => 'auto']);
+        }
     }
 }
